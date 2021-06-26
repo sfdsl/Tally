692cc38441d2939d6fb1c7e4320db7c421997a24
sfdsl <923125498@qq.com>
2021-06-26 19:56:11 +0800

    Second commit .

---
+++ 记账本/java/com/hui/tally/RecordActivity.java
@@ -0,0 +1,17 @@
+package com.hui.tally;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+    public void onClick(View view) {
+    }
+}
---
+++ 记账本/java/com/hui/tally/frag_record/OutcomeFragment.java
@@ -0,0 +1,32 @@
+package com.hui.tally.frag_record;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hui.tally.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OutcomeFragment extends Fragment {
+
+
+    public OutcomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_outcome, container, false);
+    }
+
+}
